d275744df402a3905d00aeb3a2f00f3e6301edc4
ChilenoSika <jaum97@yahoo.com.br>
2019-02-17 23:33:46 -0300

    Middleware

--- index.js
+++ index.js
@@ -9,11 +9,19 @@ nunjucks.configure('views', {
   watch: true
 })
 
+const ageMiddleware = (req, res) => {
+  req.body.age
+    ? res.render(req.body.age > 17 ? 'major' : 'minor')
+    : res.render('main')
+}
+
 app.use(express.urlencoded({ extended: false }))
 app.set('view engine', 'njk')
 
+app.post('/check', ageMiddleware, (req, res) => {})
+
 app.get('/', (req, res) => {
-  return res.render('main')
+  return res.render('main', { age: 0 })
 })
 
 app.listen(3000)
